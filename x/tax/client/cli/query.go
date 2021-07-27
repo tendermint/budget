465967f7946994d2d442e81924ab4c23ad318cbe
dongsam <dongsamb@gmail.com>
2021-07-27 19:37:51 +0900

    add param query endpoint on cli

--- x/tax/client/cli/query.go
+++ x/tax/client/cli/query.go
@@ -1,7 +1,13 @@
 package cli
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/spf13/cobra"
 
 	"github.com/tendermint/tax/x/tax/types"
@@ -18,10 +24,49 @@ func GetQueryCmd() *cobra.Command {
 	}
 
 	taxQueryCmd.AddCommand(
+	GetCmdQueryParams(),
 	// TODO: add query commands
-	// GetCmdQueryParams(),
 	// GetCmdQueryTaxes(),
 	)
 
 	return taxQueryCmd
+}
+
+// GetCmdQueryParams implements the params query command.
+func GetCmdQueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the values set as tax parameters",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as tax parameters.
+
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.Params(
+				context.Background(),
+				&types.QueryParamsRequest{},
+			)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
 }
--- x/tax/types/tax.go
+++ x/tax/types/tax.go
@@ -1,5 +1,20 @@
 package types
 
-func (t Tax) String() string {
-	return t.String()
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+func (tax Tax) String() string {
+	out, _ := tax.MarshalYAML()
+	return out.(string)
+}
+
+// MarshalYAML returns the YAML representation of an Tax.
+func (tax Tax) MarshalYAML() (interface{}, error) {
+	bz, err := codec.MarshalYAML(codec.NewProtoCodec(codectypes.NewInterfaceRegistry()), &tax)
+	if err != nil {
+		return nil, err
+	}
+	return string(bz), err
 }
