d750446225fa22184dd1fff6d73c3ffa07649f48
kogisin <jaybkim2@gmail.com>
2021-09-27 18:06:18 +0900

    feat: adding events work in progress

--- x/budget/types/events.go
+++ x/budget/types/events.go
@@ -2,7 +2,14 @@ package types
 
 // Event types for the budget module.
 const (
-	EventTypeAddBudget    = "add_budget"
-	EventTypeDeleteBudget = "delete_budget"
-	EventTypeUpdateBudget = "update_budget"
+	EventTypeAddBudget       = "add_budget"
+	EventTypeDeleteBudget    = "delete_budget"
+	EventTypeUpdateBudget    = "update_budget"
+	EventTypeBudgetCollected = "budget_collected"
+
+	AttributeValueName                = "name"
+	AttributeValueCollectionAddress   = "collection_address"
+	AttributeValueBudgetSourceAddress = "budget_source_address"
+	AttributeValueRate                = "rate"
+	AttributeValueAmount              = "amount"
 )
