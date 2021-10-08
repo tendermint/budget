e965b503ac94be8951908337801085d562788b96
kogisin <jaybkim2@gmail.com>
2021-10-08 18:19:13 +0900

    test: add params and test code

--- x/budget/simulation/params.go
+++ x/budget/simulation/params.go
@@ -1,5 +1,7 @@
 package simulation
 
+// DONTCOVER
+
 import (
 	"fmt"
 	"math/rand"
@@ -10,8 +12,6 @@ import (
 	"github.com/tendermint/budget/x/budget/types"
 )
 
-// DONTCOVER
-
 // ParamChanges defines the parameters that can be modified by param change proposals
 // on the simulation
 func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
@@ -21,12 +21,10 @@ func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 				return fmt.Sprintf("%d", GenEpochBlocks(r))
 			},
 		),
-		// TODO: Randomize structured ParamChange
-		// ref. https://github.com/cosmos/cosmos-sdk/blob/49102b1d988f542c4293af7a85e403d858f348a8/x/gov/simulation/params.go#L39
-		//simulation.NewSimParamChange(types.ModuleName, string(types.KeyBudgets),
-		//	func(r *rand.Rand) string {
-		//		return fmt.Sprintf("%s", GenBudgets(r))
-		//	},
-		//),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyBudgets),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("%s", GenBudgets(r))
+			},
+		),
 	}
 }
