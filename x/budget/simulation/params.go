89fdd934cc18ede99de386ab9ef9ad4f5408bb0b
JayB <jaybkim2@gmail.com>
2021-10-13 15:24:43 +0900

    test: update simulation tests (#54)
    * chore: randomize blokcs and budgets and add genesis test
    
    * test: add params and test code
    
    * test: work in progress...decision making is needed
    
    * chore: apply from code review
    
    * chore: remove unused comments and clean up
    
    * fix: broken test

--- x/budget/simulation/params.go
+++ x/budget/simulation/params.go
@@ -1,6 +1,9 @@
 package simulation
 
+// DONTCOVER
+
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 
@@ -9,8 +12,6 @@ import (
 
 	"github.com/tendermint/budget/x/budget/types"
 )
-
-// DONTCOVER
 
 // ParamChanges defines the parameters that can be modified by param change proposals
 // on the simulation
@@ -21,12 +22,14 @@ func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 				return fmt.Sprintf("%d", GenEpochBlocks(r))
 			},
 		),
-		// TODO: Randomize structured ParamChange
-		// ref. https://github.com/cosmos/cosmos-sdk/blob/49102b1d988f542c4293af7a85e403d858f348a8/x/gov/simulation/params.go#L39
-		//simulation.NewSimParamChange(types.ModuleName, string(types.KeyBudgets),
-		//	func(r *rand.Rand) string {
-		//		return fmt.Sprintf("%s", GenBudgets(r))
-		//	},
-		//),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyBudgets),
+			func(r *rand.Rand) string {
+				bz, err := json.Marshal(GenBudgets(r))
+				if err != nil {
+					panic(err)
+				}
+				return string(bz)
+			},
+		),
 	}
 }
